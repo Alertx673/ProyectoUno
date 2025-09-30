7edbc52d72ea8aee178e9e962b1a58c269c3de86
Alex Vve <gsupet@gmail.com>
2025-09-30 12:06:39 -0500

    trabajo de clases estudiante y curso

---
+++ src/main/java/ec/edu/espoch/proyectouno/Curso.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.proyectouno;
+
+/**
+ *
+ * @author gsupe
+ */
+public class Curso {
+    
+    //Atributos
+    public String nombreCurso;
+    public int codigo;
+    public String listaEstudiante;
+    
+    //metodos
+    public void agregarEstudiante(){
+        
+    }
+    
+    public void eliminarEstudiante(){
+    
+    
+    }
+    
+    public void mostrarEstudiante(){
+    
+    
+    }
+    
+}
---
+++ src/main/java/ec/edu/espoch/proyectouno/Estudiante.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.proyectouno;
+
+/**
+ *
+ * @author gsupe
+ */
+public class Estudiante {
+    
+    //Atributos
+    public String nombreCurso;
+    public int codigo;
+    public String listaEstudiante;
+    
+    //metodos
+    public void agregarEstudiante(){
+        
+    }
+    
+    public void eliminarEstudiante(){
+    
+    
+    }
+    
+    public void mostrarEstudiante(){
+    
+    
+    }
+    
+  
+    
+}
